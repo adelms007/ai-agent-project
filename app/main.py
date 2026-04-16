3e602de2a65bef486dda30b8ca3ce5d1a982e91a
adel mensi <adelmensi8@gmail.com>
2026-04-16 20:25:32 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -19,11 +19,44 @@ def main():
 
     client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
-    chat = client.chat.completions.create(
-        model="anthropic/claude-haiku-4.5",
-        messages=[{"role": "user", "content": args.p}],
-        max_tokens=1000,
-        tools=[{
+    # chat = client.chat.completions.create(
+    #     model="anthropic/claude-haiku-4.5",
+    #     messages=[{"role": "user", "content": args.p}],
+    #     max_tokens=1000,
+    #     tools=[{
+    #         "type": "function",
+    #         "function": {
+    #             "name": "Read",
+    #             "description": "Read and return the contents of a file",
+    #             "parameters": {
+    #             "type": "object",
+    #             "properties": {
+    #                 "file_path": {
+    #                 "type": "string",
+    #                 "description": "The path to the file to read"
+    #                 }
+    #             },
+    #             "required": ["file_path"]
+    #             }
+    #         }
+    #         }],
+    # )
+
+
+    # if not chat.choices or len(chat.choices) == 0:
+    #     raise RuntimeError("no choices in response")
+
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!", file=sys.stderr)
+
+    # TODO: Uncomment the following line to pass the first stage
+    # This will show you the 'tool_calls' if the model decided to use the tool
+
+
+
+
+    message=[{"role": "user", "content": args.p}]
+    tool = [{
             "type": "function",
             "function": {
                 "name": "Read",
@@ -39,41 +72,88 @@ def main():
                 "required": ["file_path"]
                 }
             }
-            }],
+            }]
+
+    chat = client.chat.completions.create(
+        model="anthropic/claude-haiku-4.5",
+        messages=message,
+        max_tokens=1000,
+        tools=tool,
     )
 
     if not chat.choices or len(chat.choices) == 0:
         raise RuntimeError("no choices in response")
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+    while (chat.choices[0].message.tool_calls) :
+        payload = chat.choices[0].message
+        message.append(payload.model_dump())
+        if (chat.choices[0].message.content != None):
+            print(chat.choices[0].message.content)
 
-    # TODO: Uncomment the following line to pass the first stage
-    # This will show you the 'tool_calls' if the model decided to use the tool
-
-
-    #print(chat.choices[0].message.content)
-
-    if chat.choices[0].message.tool_calls:
-        tool_call = chat.choices[0].message.tool_calls[0]
-    
-        if tool_call.function.name == "Read":
-            args_string = tool_call.function.arguments
-            args_dict = json.loads(args_string)
-            file_path = args_dict["file_path"]
+        
+        for tool_call in payload.tool_calls :
+            result = "Error: Tool not recognized"
+            if tool_call.function.name == "Read":
+                args_string = tool_call.function.arguments
+                args_dict = json.loads(args_string)
+                file_path = args_dict["file_path"]
+                result = "file not found"
+                
+                try:
+                    with open(file_path, "r", encoding="utf-8") as file:
+                        result = file.read()
+                        
+                except FileNotFoundError:
+                    print(f"Error: The file {file_path} does not exist.", file=sys.stderr)
+                except Exception as e:
+                    print(f"An error occurred: {e}", file=sys.stderr)
             
-            try:
-                with open(file_path, "r", encoding="utf-8") as file:
-                    content = file.read()
-                    # Print to stdout for the test runner to see
-                    print(content)
-            except FileNotFoundError:
-                print(f"Error: The file {file_path} does not exist.", file=sys.stderr)
-            except Exception as e:
-                print(f"An error occurred: {e}", file=sys.stderr)
-    else:
-    # If no tool was called, show the text response
-        print(chat.choices[0].message.content)
+            message.append({
+                "role": "tool",
+                "tool_call_id": tool_call.id,
+                "content": result
+            })
+        
+        chat = client.chat.completions.create(
+        model="anthropic/claude-haiku-4.5",
+        messages=message,
+        max_tokens=1000,
+        tools=tool,
+        )
+
+
+    print(chat.choices[0].message.content)
+        
+
+
+
+
+
+
+    # print(chat)
+
+    # if chat.choices[0].message.tool_calls:
+    #     tool_call = chat.choices[0].message.tool_calls[0]
+    
+    #     if tool_call.function.name == "Read":
+    #         args_string = tool_call.function.arguments
+    #         args_dict = json.loads(args_string)
+    #         file_path = args_dict["file_path"]
+             
+    #         try:
+    #             with open(file_path, "r", encoding="utf-8") as file:
+    #                 content = file.read()
+    #                 # Print to stdout for the test runner to see
+    #                 print(content)
+    #         except FileNotFoundError:
+    #             print(f"Error: The file {file_path} does not exist.", file=sys.stderr)
+    #         except Exception as e:
+    #             print(f"An error occurred: {e}", file=sys.stderr)
+    # else:
+    # # If no tool was called, show the text response
+    #     print(chat.choices[0].message.content)
+
+    
     
 
 
